33e78de7a5a110326ac4025e502650f06b9a67c1
Andra Blaj <andra@medic.org>
2024-06-28 15:26:35 +0300

    Try to fix GA

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -37,7 +37,7 @@ export default function App({ Component, pageProps, router }: CustomAppProps) {
       </Script>
 
       <Head>
-        <title>{formatTitle()}TEST</title>
+        <title>{formatTitle()}</title>
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href={assetUrl("/favicon.ico")} />
       </Head>
